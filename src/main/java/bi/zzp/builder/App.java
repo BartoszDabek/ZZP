0e7ede5e7a6fe05ddeb113b2051a4cd438b1f214
Bartosz Dąbek <elderaramka@gmail.com>
2019-10-21 22:04:39 +0200

    add builder class

--- src/main/java/bi/zzp/builder/App.java
+++ src/main/java/bi/zzp/builder/App.java
@@ -1,12 +1,8 @@
 package bi.zzp.builder;
 
-import bi.zzp.builder.model.Dough;
 import bi.zzp.builder.model.Pizza;
-import bi.zzp.builder.model.Sauce;
-import bi.zzp.builder.model.Topping;
+import bi.zzp.builder.model.PizzaBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -19,47 +15,38 @@ public class App {
     }
 
     private void addPizza() {
-        try(Scanner in = new Scanner(System.in)) {
+        try (Scanner in = new Scanner(System.in)) {
+            PizzaBuilder pizzaBuilder = new PizzaBuilder();
             System.out.println("What type of dough do you want? (ITALIAN, POLISH, AMERICAN)");
-            String doughValue = in.nextLine();
-            if (doughValue.isBlank()) {
-                doughValue = "italian";
-            }
-            Dough dough = Dough.valueOf(doughValue);
+            String dough = in.nextLine();
 
             System.out.println("What type of sauce? (GARLIC, BBQ, CHILI)");
-            String sauceValue = in.nextLine();
-            if (sauceValue.isBlank()) {
-                sauceValue = "bbq";
-            }
-            Sauce sauce = Sauce.valueOf(sauceValue);
+            String sauce = in.nextLine();
 
             System.out.println("What pizza do you want? (Firm, Margherita, Chicken, etc..)");
             String name = in.nextLine();
-            if (name.isBlank()) {
-                name = "margherita";
-            }
 
             System.out.println("What size of pizza? (SMALL, MEDIUM, LARGE, EXTRA)");
             String size = in.nextLine();
-            if (size.isBlank()) {
-                size = "medium";
-            }
 
-            List<Topping> toppings = new ArrayList<>();
             while (true) {
                 System.out.println("What topping do you want?");
-                toppings.add(Topping.valueOf(in.nextLine()));
+                pizzaBuilder.addTopping(in.nextLine());
 
                 System.out.println("Next topping? (YES/NO)");
                 String nextTopping = in.nextLine();
-
                 if ("NO".equalsIgnoreCase(nextTopping)) {
                     break;
                 }
             }
 
-            Pizza pizza = new Pizza(dough, sauce, toppings, name, size);
+            Pizza pizza = pizzaBuilder
+                    .withDough(dough)
+                    .withSauce(sauce)
+                    .withName(name)
+                    .withSize(size)
+                    .build();
+
             System.out.println(pizza);
         }
     }
---
+++ src/main/java/bi/zzp/builder/model/PizzaBuilder.java
@@ -0,0 +1,53 @@
+package bi.zzp.builder.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PizzaBuilder {
+    private List<Topping> toppings = new ArrayList<>();
+    private String size;
+    private String name;
+    private Sauce sauce;
+    private Dough dough;
+
+    public PizzaBuilder addTopping(String topping) {
+        this.toppings.add(Topping.valueOf(topping));
+        return this;
+    }
+
+    public PizzaBuilder withSize(String size) {
+        if (size.isBlank()) {
+            size = "medium";
+        }
+        this.size = size;
+        return this;
+    }
+
+    public PizzaBuilder withName(String name) {
+        if (name.isBlank()) {
+            name = "margherita";
+        }
+        this.name = name;
+        return this;
+    }
+
+    public PizzaBuilder withSauce(String sauceValue) {
+        if (sauceValue.isBlank()) {
+            sauceValue = "bbq";
+        }
+        this.sauce = Sauce.valueOf(sauceValue);
+        return this;
+    }
+
+    public PizzaBuilder withDough(String doughValue) {
+        if (doughValue.isBlank()) {
+            doughValue = "italian";
+        }
+        this.dough = Dough.valueOf(doughValue);
+        return this;
+    }
+
+    public Pizza build() {
+        return new Pizza(dough, sauce, toppings, name, size);
+    }
+}
